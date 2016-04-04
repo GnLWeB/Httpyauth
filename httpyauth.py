1ba5ac0f79531bc2aa6ee093a3b1e60cf4c7af85
RagingWeb <gnlw3b@gmail.com>
2016-04-04 16:51:33 +0200

    Add license informations

--- httpyauth.py
+++ httpyauth.py
@@ -1,3 +1,17 @@
+/*
+ *	httpyauth 0.1
+ *
+ *
+ *	This module:
+ *		This module is free software; you can redistribute it and/or
+ *		modify it under the terms of the GNU General Public License
+ *		as published by the Free Software Foundation; either version
+ *		2 of the License, or (at your option) any later version.
+ *
+ *	History
+ *	2016-04-04	RagingWeb	first commit
+ */
+
 import BaseHTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 import sys
